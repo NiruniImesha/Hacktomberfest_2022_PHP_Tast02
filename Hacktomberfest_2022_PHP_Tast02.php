eb3dfcc5bea7f02948860b1fb36948a4dca72469
Akila Tharuka <76250866+Akilatharuka97@users.noreply.github.com>
2022-10-18 10:49:19 +0530

    Create Hacktomberfest_2022_PHP_Tast02.php

---
+++ Hacktomberfest_2022_PHP_Tast02.php
@@ -0,0 +1,17 @@
+<?php
+$favcolor = "red";
+
+switch (favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    eco "Your favorite color is blue!";
+    break;
+  case "green":
+    eco "Your favorite color is green!";
+    break;
+  default:
+    eco "Your favorite color is neither red, blue, nor green!";
+}
+?>
